30b11b56708d46cb795c06d2fc447b8d9c66e58c
Minseok Lee <korpenta@gmail.com>
2025-01-29 02:00:58 +0900

    feat: 유저 엔티티 정의

---
+++ src/main/java/com/dogGetDrunk/meetjyou/user/User.java
@@ -0,0 +1,50 @@
+package com.dogGetDrunk.meetjyou.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TINYINT(1) DEFAULT 1")
+    private boolean notified;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime lastLoginAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private String email;
+    private String nickname;
+    private int gender;
+    private int age;
+    private String bio;
+    private String imgUrl;
+    private String thumbImgUrl;
+    private AuthProvider authProvider;
+    private UserStatus status;
+    private Role role;
+}
---
+++ src/main/java/com/dogGetDrunk/meetjyou/user/UserStatus.java
@@ -0,0 +1,5 @@
+package com.dogGetDrunk.meetjyou.user;
+
+public enum UserStatus {
+    NORMAL, DELETED
+}
