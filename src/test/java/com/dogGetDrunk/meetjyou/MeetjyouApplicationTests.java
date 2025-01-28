bb8078131a0f22701160e159b010916ab8c1a5ec
Minseok Lee <korpenta@gmail.com>
2025-01-29 01:39:02 +0900

    프로젝트 기본 세팅

---
+++ src/test/java/com/dogGetDrunk/meetjyou/MeetjyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogGetDrunk.meetjyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetjyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
