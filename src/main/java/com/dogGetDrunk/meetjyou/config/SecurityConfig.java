17645bd336159f645e45831a2e3e54b19c003684
damiannlee <korpenta@gmail.com>
2025-02-06 00:51:37 +0900

    feat: CORS 설정 구현
    모든 origin에 대해 접근 허용

--- src/main/java/com/dogGetDrunk/meetjyou/config/SecurityConfig.java
+++ src/main/java/com/dogGetDrunk/meetjyou/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.dogGetDrunk.meetjyou.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,17 +10,21 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CorsConfig corsConfig;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").authenticated() // Swagger UI 접근 시 인증 요구
-                .anyRequest().permitAll()
-            )
-            .httpBasic(withDefaults()) // Basic Auth 적용
-            .csrf(AbstractHttpConfigurer::disable); // CSRF 보호 비활성화 (필요 시 설정)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").authenticated() // Swagger UI 접근 시 인증 요구
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults()) // Basic Auth 적용
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable); // CSRF 보호 비활성화 (필요 시 설정)
 
         return http.build();
     }
