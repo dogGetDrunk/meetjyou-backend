ee85adfc85efa7ea936aeb853be0ec34b395cbb5
damiannlee <korpenta@gmail.com>
2025-02-17 17:30:13 +0900

    [feat] 앱 버전 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -156,6 +156,15 @@ CREATE TABLE user_party
     user_id   INT
 );
 
+CREATE TABLE app_version
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    version VARCHAR(20) NOT NULL UNIQUE,
+    force_update TINYINT NOT NULL,
+    download_url VARCHAR(300) NOT NULL,
+    release_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 ALTER TABLE post
     ADD FOREIGN KEY (user_id) REFERENCES user (id);
 
