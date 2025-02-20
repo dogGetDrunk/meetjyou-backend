c3ceae7bf625a3b50e7eafcd05217ec9c8205195
damiannlee <korpenta@gmail.com>
2025-02-20 17:48:07 +0900

    [build] 서버 및 스웨거 설정 분리
    `SwaggerConfig`에서 ngrok url을 정적으로 관리하고 있었는데, 이는 9090 포트에 뜨는 릴리즈 서버에만 필요했음.
    8080 포트에 뜨는 개발 서버에서 같은 설정으로 테스트를 진행 중 정적으로 등러간 ngrok url이 적용되어
    localhost:8080이 아닌 ngrok.something:9090 으로 요청이 전송되었고, 아직 릴리즈되지 않은 기능이므로 500 에러가 남.
    이에 릴리즈 및 개발 서버 설정과 그에 따른 스웨거 설정도 분리함.
    이를 활용하기 위해 인텔리제이 Run configuration에 `-Dspring.profiles.active=dev,db,secrets` 등을 추가해야 함.

--- src/main/java/com/dogGetDrunk/meetjyou/config/SwaggerReleaseConfig.java
+++ src/main/java/com/dogGetDrunk/meetjyou/config/SwaggerReleaseConfig.java
@@ -8,9 +8,11 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
-public class SwaggerConfig {
+@Profile("release")
+public class SwaggerReleaseConfig {
 
     @Value("${ngrok.url}")
     private String ngrokUrl;
