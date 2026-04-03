39b6949b385113d1240f4f5aae0e3ae0ca13428a
damiannlee <korpenta@gmail.com>
2026-04-04 01:01:17 +0900

    refactor: websocket stomp endpoint allowed pattern 분리
    보안성 향상을 위해 release/dev용 endpoint 분리

--- src/main/java/com/dogGetDrunk/meetjyou/config/WebSocketConfig.kt
+++ src/main/java/com/dogGetDrunk/meetjyou/config/WebSocketConfig.kt
@@ -1,6 +1,7 @@
 package com.dogGetDrunk.meetjyou.config
 
 import com.dogGetDrunk.meetjyou.chat.connection.ChatStompInterceptor
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
@@ -12,6 +13,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 class WebSocketConfig(
     private val chatStompInterceptor: ChatStompInterceptor,
+    @Value("\${app.websocket.allowed-origin-patterns}")
+    private val allowedOriginPatterns: List<String>,
 ) : WebSocketMessageBrokerConfigurer {
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
@@ -21,7 +24,7 @@ class WebSocketConfig(
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/ws-chat")
-            .setAllowedOriginPatterns("*")
+            .setAllowedOriginPatterns(*allowedOriginPatterns.toTypedArray())
     }
 
     override fun configureClientInboundChannel(registration: ChannelRegistration) {
